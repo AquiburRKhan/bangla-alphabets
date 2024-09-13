8fdafdee0ecd95e0bc5c9e75755b823498284db2
AquiburRahmanKhan <aquibur.rahman@monstar-lab.com>
2024-09-13 16:52:53 +0600

    feat: added AR support

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { XR, XROrigin, createXRStore } from "@react-three/xr";
 
 import { Canvas } from "@react-three/fiber";
 import App from "./App.tsx";
 import "./index.css";
 
+const store = createXRStore();
+
 const root = createRoot(document.querySelector("#root")!);
+
+const handleEnterArClick = async () => {
+  if (window.navigator.xr) {
+    const isARSupported = await window.navigator.xr.isSessionSupported(
+      "immersive-ar"
+    );
+
+    if (isARSupported) {
+      store.enterAR();
+    } else {
+      window.alert(
+        "AR not supported, try opening the page using a recent version of Chrome on Android."
+      );
+    }
+  }
+};
 
 root.render(
   <StrictMode>
+    <button
+      style={{
+        padding: "10px",
+      }}
+      onClick={handleEnterArClick}
+    >
+      Enter AR
+    </button>
     <Canvas
       camera={{
         fov: 75,
@@ -17,7 +44,10 @@ root.render(
         position: [0, 0, 10],
       }}
     >
-      <App />
+      <XR store={store}>
+        <XROrigin position-y={6} position-z={30} />
+        <App />
+      </XR>
     </Canvas>
   </StrictMode>
 );
